4c9ce1bddcdfa3d148b0906db6109e64aea0eff7
MAT34525 <antoine.le.chevalier@proton.me>
2023-12-17 11:43:26 +0200

    AES Load data / Load key / Export Data
    + AES Load data / Export data
    
    + Simplify transformation methods : Blocks > Sequence / Sequence > Blocks
    
    + Test on files & random data

--- AES.py
+++ AES.py
@@ -1,23 +1,36 @@
 import warnings
 from Block import Block
 from Key import Key
+from random import randbytes
+from tqdm import tqdm
 
 class AES :
 
-    def __init__(self, sequence : bytearray, cipherKey : bytearray) :
+    def __init__(self, sequence : bytearray = None, cipherKey : bytearray = None) :
         """
         Create a new AES object
         'sequence' - Sequence of bytes to be ciphered / unciphered
         'cipherKey' -  Sequence of 16 / 24 / 32 bytes used as a key
         """
-            
-        # Verify the key length
-        if len(cipherKey) not in [16, 24, 32]:
 
-            warnings.warn("Wrong number of bytes in the key")
-
-            return -1
+        self.blocks = None
+        self.key = None
         
+        if sequence != None:
+
+            self.blocks = self.SplitSequenceInBlocks(sequence)
+
+        if cipherKey != None :
+
+            self.Key = Key(cipherKey)
+
+    # Static methods & Other =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+            
+    def SequenceToBlocks(sequence : bytearray) -> list:
+        """
+        Convert a sequence of bytes into multiple blocks
+        return : Array of Block
+        """
         # Divide the byte sequence in multiple blocks of 15 bytes
         nBlocks = len(sequence) // 16 if len(sequence)%16 == 0 else len(sequence) // 16 + 1
 
@@ -27,9 +40,192 @@ class AES :
         for blockIndex in range(nBlocks):
 
             blocks.append(Block(sequence[blockIndex * 16 : (blockIndex + 1) * 16 ]))
+
+        return blocks
+
+    def BlocksToSequence(self) -> list:
+        """
+        Convert a block array to an array of bytes
+        return : Sequence of bytes
+        """
+        sequence = []
+
+        for block in self.blocks:
+
+            # Get byte sequence from each block and add it to the sequence
+            sequence.extend(block.BlockToSequence())
+
+        return sequence
+
+    def PrintSequence(sequence : bytearray):
+        """
+        Print an array of bytes using the hexadecimal format
+        """
+
+        print(" ".join([hex(x)[2:] if len(hex(x)) == 4 else "0" + hex(x)[2:]  for x in sequence]))
+
+    # Instance methods =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    # <=-=-=-= Load the data =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+    def FromRandomByte(self, size : int):
+        """
+        Generate a random sequence of bytes with a specific size to be encrypted
+        'size' - Size of the sequence to be encrypted
+        """
+
+        # Generate randoms bytes
+        sequence = randbytes(size)
+
+        # Create blocks from byte sequence
+        self.blocks = AES.SequenceToBlocks(sequence)
+
+        print("Randomly generated sequence :")
+        AES.PrintSequence(sequence)
+
+    def FromText(self, textSequence : str):
+        """
+        Convert the 'textSequence' to an array of bytes to be converted to blocks
+        'textKey' - String to be converted and used as a key
+        """
+
+        # Convert characters to their byte equivalent
+        byteSequence = [ord(x) for x in textSequence]
+
+        # Convert the byte sequence to blocks
+        self.blocks = AES.SequenceToBlocks(byteSequence)
+
+        print("Byte sequence :")
+        AES.PrintSequence(byteSequence)
         
-        self.blocks = blocks
-        self.key = Key(cipherKey)
+    def FromFile(self, path):
+        """
+        Get the bytes from a file located at 'path' and convert it to blocks
+        'path' - String to be used as a relative path to the file
+        """
+
+        # Open the file and get the byte arrays
+        with open(path, "rb") as f:
+
+            sequence = f.read()
+            f.close()
+
+        # Convert the sequence to blocks
+        self.blocks = AES.SequenceToBlocks(sequence)
+
+    # <=-=-=-= Load the key =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    def UseRandomKey(self, size : int):
+        """
+        Generate a random sequence of bytes with a specific size to be used as a key
+        'size' - Size of the sequence to be used as a key
+        """
+
+        if size not in [16, 24, 32]:
+
+            raise ValueError("The key size is invalid and should be 16, 24 or 32 bytes")
+
+        self.key = Key(randbytes(size))
+
+        print("Randomly generated key :")
+        AES.PrintSequence(self.key.key)
+
+    def UseBytesKey(self, key : list):
+        """
+        Get the bytes to be used as a key
+        'key' - Integer array to be used as a key
+        """
+
+        if len(key) not in [16, 24, 32]:
+
+            raise ValueError("The key size is invalid and should be 16, 24 or 32 bytes")
+
+        self.key = Key(key)
+
+        print("Input key :")
+        AES.PrintSequence(key)
+
+    def UseTextKey(self, textKey : str):
+        """
+        Convert the 'textKey' to an array of bytes to be used as a key
+        'textKey' - String to be converted and used as a key
+        """
+
+        byteKey = [ord(x) for x in textKey]
+
+        if len(byteKey) < 16 :
+
+            raise ValueError("Text used to generate a key is too short, should be >= 16")
+        
+        keySize = len(byteKey)
+        
+        # Cast the key into an array of bytes depending on it's length
+        if keySize <= 24 :
+
+            key = byteKey[:16]
+
+        elif keySize <= 32 :
+
+            key = byteKey[:24]
+
+        else :
+
+            key = byteKey[:32]
+
+        self.key = Key(key)
+
+        print("Input key :")
+        AES.PrintSequence(self.key.key)
+
+    # <=-=-=-= Export the data =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        
+    def ToBytes(self):
+        """
+        Convert the blocks to bytes
+        """
+
+        # Get the sequence from blocks
+        sequence = self.BlocksToSequence()
+
+        print("Output byte sequence :")
+        print(AES.PrintSequence(sequence))
+
+    def ToText(self):
+        """
+        Convert the blocks to a text
+        """
+
+        # Get sequence from blocks
+        sequence = self.BlocksToSequence()
+
+        # Get the characters from their bytes equivalent
+        text = "".join(chr(x) for x in sequence)
+
+        print("Output text :")
+        print(text)
+
+    def ToFile(self, path):
+        """
+        Convert the blocks to a file
+        'path' - String to be used as a path where the file will be created 
+        """
+
+        # Get bytes sequence from blocks
+        sequence = self.BlocksToSequence()
+
+        # Convert bytes sequence to a signle byte array
+        bytesSequence = bytes(list(sequence))
+
+        # Write the bytesSequence to the file
+        with open(path, "wb+") as f:
+
+            f.write(bytesSequence)
+            f.close()
+
+        print("Output file has been created at :")
+        print(path)
+
+    # <=-=-=-= Encryption / Decryption =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     def CipherBlock(self, block : Block):
         """
@@ -55,6 +251,26 @@ class AES :
         block.ShiftRows()
         block.AddRoundKey(self.key.Nb, keySchedule)
 
+        return block
+
+    def Cipher(self):
+        """
+        Encrypt all blocks using the key        
+        """
+
+        nBlocks = len(self.blocks)
+
+        encryptedBlocks = []
+
+        # Iteration with progress bar 
+        for blockIndex in tqdm(range(nBlocks), desc= "Encrypt blocks...") :
+
+            # Encrypt a block
+            encryptedBlocks.append(self.CipherBlock(self.blocks[blockIndex]))
+
+        self.blocks = encryptedBlocks
+
+
     def UnCipherBlock(self, block : Block):
         """
         Decrypt a block of 16 bytes using the AES using the key
@@ -78,15 +294,24 @@ class AES :
         block.InvSubBytes()
         block.AddRoundKey(0, keySchedule)
 
+        return block
 
-    def Cipher(self):
+    def UnCipher(self):
+        """
+        Decrypt all blocks using the key
+        """
 
-        print(0)
+        nBlocks = len(self.blocks)
 
+        plainBlocks = []
 
-    def Uncipher(self):
+        # Iteration with progress bar
+        for blockIndex in tqdm(range(nBlocks), desc= "Decrypt blocks...") :
 
-        print(0)
+            # Decrypt a block
+            plainBlocks.append(self.UnCipherBlock(self.blocks[blockIndex]))
+
+        self.blocks = plainBlocks
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,61 @@
 from AES import *
 from random import randbytes
 
+def test_AES():
+
+    NewAES = AES()
+
+    KB = 1024
+    MB = 1024*1024
+    GB = 1024*1024*1024
+
+    testText = "This is a test for text encryption using AES"
+
+    NewAES.FromText("Tis is a test for text encryption using AES")
+    NewAES.UseTextKey("Hello there, my key")
+
+    print(NewAES.ToText())
+
+    NewAES.Cipher()
+
+    print(NewAES.ToText())
+
+    NewAES.UnCipher()
+
+    print(NewAES.ToText())
+
+
+
 if __name__ == "__main__" :
 
     if True:
 
-        print(type([]))
+        path = "test.jpg"
+
+        # NewAES = AES()
+
+        # NewAES.FromFile("InFiles/test.jpg")
+        # NewAES.UseTextKey("This is my key for AES encryption.txt")
+
+        # NewAES.ToFile("InFiles/test.jpg")
+
+        # NewAES.Cipher()
+
+        # NewAES.ToFile("OutFiles/test_cipher.jpg")
+
+
+        SecAES = AES()
+
+        SecAES.FromFile("OutFiles/test_cipher.jpg")
+        SecAES.UseTextKey("This is my wrong key for AES encryption.txt")
+
+        SecAES.UnCipher()
+
+        SecAES.ToFile("OutFiles/test_uncipher_with_wrong_key.jpg")
+
+
+
+
+
 
          
